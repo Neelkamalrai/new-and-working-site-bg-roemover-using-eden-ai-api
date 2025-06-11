dbb4af08eb4dec04464fb3cd0f9b1269d03890c5
neel rai <myneelpc@gmail.com>
2025-06-11 05:04:23 +0000

    Console Error
    In HTML, whitespace text nodes cannot be a child of <htm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark"> {/* Ensure dark theme is applied if :root is not solely relied upon */}
+    <html lang="en" className="dark">{/* Ensure dark theme is applied if :root is not solely relied upon */}
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
